50a12357e85b761eebbc5a217065443c41f681c5
Samuel C. Iheadindu <h1samuel@yahoo.com>
2021-12-09 12:39:54 +0100

    Add BatteryLog Entity

---
+++ src/main/java/com/samueliheadindu/assignment/entity/BatteryLog.java
@@ -0,0 +1,42 @@
+package com.samueliheadindu.assignment.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "battery_log")
+@Entity
+@Setter
+@Getter
+@ToString
+public class BatteryLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "battery_level")
+    private Long batteryLevel;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "drone_id")
+    private Drone drone;
+
+}
