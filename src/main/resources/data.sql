3a609277138f33d5697c0c2c16e2cc17d6d9b0d8
Samuel C. Iheadindu <h1samuel@yahoo.com>
2021-12-09 11:02:24 +0100

    Add seed data for medications

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO medications(name, weight, code, image) VALUES
+('Paracetamol', 100, 'PARA2X219', 'paracetamol.jpeg'),
+('Metformin', 100, 'METFX278', 'metformin.jpg'),
+('Zitromax', 100, 'ZITP19VA', 'zitromax.jpeg'),
+('Paquinol', 200, 'PLA21IM5', 'plaquinol.jpeg'),
+('Albuterol', 100, 'ALBA2X291', 'albuterol.jpeg'),
+('Emvite', 50, 'EMVFX287', 'emvite.jpg'),
+('Lipitor', 100, 'LIP19VA1', 'lipitor.jpeg'),
+('Lisinopril', 300, 'LIS213IN', 'lisinopril.jpeg'),
+('Nivaquine', 100, 'NIV26VS1', 'nivaquine.jpeg'),
+('Gabapentin', 300, 'GABS21PRN', 'gabapentin.jpeg');
