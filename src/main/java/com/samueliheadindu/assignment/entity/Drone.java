57a432b89ec1d62d4b8692339c372eef3c6e2935
Samuel C. Iheadindu <h1samuel@yahoo.com>
2021-12-09 12:40:57 +0100

    Add relationship to Drone

--- src/main/java/com/samueliheadindu/assignment/entity/Drone.java
+++ src/main/java/com/samueliheadindu/assignment/entity/Drone.java
@@ -1,11 +1,16 @@
 package com.samueliheadindu.assignment.entity;
 
 import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -46,4 +51,10 @@ public class Drone {
     @UpdateTimestamp
     @Column(name = "updated_at")
     private Date updatedAt;
+
+    @OneToMany(mappedBy = "drone", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DroneLoad> items = new ArrayList<>();
+
+    @OneToMany(mappedBy = "drone", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BatteryLog> history = new ArrayList<>();
 }
