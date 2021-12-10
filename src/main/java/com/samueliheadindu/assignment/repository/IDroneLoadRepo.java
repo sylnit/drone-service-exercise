f862edf0fd3451c227abb4576d79eb2473ac0905
Samuel C. Iheadindu <h1samuel@yahoo.com>
2021-12-10 12:10:05 +0100

    Add method findByDrone()

--- src/main/java/com/samueliheadindu/assignment/repository/IDroneLoadRepo.java
+++ src/main/java/com/samueliheadindu/assignment/repository/IDroneLoadRepo.java
@@ -1,11 +1,14 @@
 package com.samueliheadindu.assignment.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.samueliheadindu.assignment.entity.DroneLoad;
+import com.samueliheadindu.assignment.entity.Drone;
 
 @Repository
 public interface IDroneLoadRepo extends JpaRepository<DroneLoad, Long> {
 
+    List<DroneLoad> findByDrone(Drone drone);
 }
