8714c5fb091655abc5362445be22c6ab7c5723fc
Samuel C. Iheadindu <h1samuel@yahoo.com>
2021-12-10 11:56:40 +0100

    Add BatteryLevel response POJO

---
+++ src/main/java/com/samueliheadindu/assignment/templates/BatteryLevelTpl.java
@@ -0,0 +1,13 @@
+package com.samueliheadindu.assignment.templates;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class BatteryLevelTpl {
+
+    private Long id;
+
+    private String batteryLevel;
+}
